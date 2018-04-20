73e5ab4f753a0210965950b9842dc68bf2107b07
Alberto Centelles <centelles.alberto@gmail.com>
2018-04-20 12:51:20 +0100

    Add verify function

--- src/LSAG.hs
+++ src/LSAG.hs
@@ -18,59 +18,61 @@ import           Protolude                    hiding (hash, head)
 secp256k1Curve :: ECC.Curve
 secp256k1Curve = ECC.getCurveByName ECC.SEC_p256k1
 
-testSignatureGen
-  :: MonadRandom m
-  => ECC.Curve
+testSignature
+  :: ECC.Curve
   -> Int
   -> ByteString
-  -> m (Integer, [Integer], ECC.Point)
-testSignatureGen curve nParticipants msg = do
+  -> IO Bool
+testSignature curve nParticipants msg = do
   -- Gen public and private keys
   (pubKey, privKey) <- ECC.generate curve
-  pubKeys' <- genNPubKeys curve nParticipants
-  sign curve pubKeys' (pubKey, privKey) msg
-
+  extPubKeys <- genNPubKeys curve nParticipants
+  -- k = position of the signer's key in the list of public keys
+  k <- fromInteger <$> generateBetween 1 (fromIntegral $ length extPubKeys - 1)
+  let pubKeys = insertPubKey k pubKey extPubKeys
+  signature <- sign curve pubKeys (pubKey, privKey) k msg
+  print signature
+  pure $ verify curve pubKeys signature msg
 
 sign
   :: MonadRandom m
   => ECC.Curve                            -- ^ Curve
-  -> [ECDSA.PublicKey]                    -- ^ List of participants public keys
+  -> [ECDSA.PublicKey]                    -- ^ List of public keys
   -> (ECDSA.PublicKey, ECDSA.PrivateKey)  -- ^ Signer's public and private keys
-  -- -> Int                               -- ^ Signer's position in list L
+  -> Int                                  -- ^ Signer's position in list L
   -> ByteString                           -- ^ Message
   -> m (Integer, [Integer], ECC.Point)
-sign curve vks' (vk, sk) msg = do
-  -- k -> position of the signer's key in the list of public keys
-  k <- fromInteger <$> generateBetween 1 (fromIntegral (length vks') - 1)
-  let vks = insertPubKey k vk vks'
-  let participants = length vks
+sign curve pubKeys (pubKey, privKey) k msg = do
+  let participants = length pubKeys
 
-  -- STEP 1
+  --------------------- STEP 1 -----------------------
   -- In ECC, h is a point in the curve. h = g x H_2(L)
   -- Compute y = h x x_pi
-  let h = ECC.pointBaseMul curve (hashPubKeys curve vks)
-      y = ECC.pointMul curve (ECDSA.private_d sk) h
+  let h = ECC.pointBaseMul curve (hashPubKeys curve pubKeys)
+      y = ECC.pointMul curve (ECDSA.private_d privKey) h
 
-  -- STEP 2
+  --------------------- STEP 2 -----------------------
   -- Pick u and compute challenge c = H(L, y, m, g^u, h^u)
   u <- generateBetween 1 (n - 1)
   let msg = "hello world"
   -- Initial challenge on k + 1
-  let initialChallenge = genChallenge curve vks y msg (gu u) (hu u) u
+  let initialChallenge = genChallenge curve pubKeys y msg (gu u) (hu u) u
 
   -- Generate L random s values
   ss <- replicateM (participants - 1) $ generateBetween 1 (n - 1)
 
+  --------------------- STEP 3 -----------------------
   -- Generate challenges
   -- Challenges start at (k + 1)
   let initialState = (((k + 1) `mod` participants, u, initialChallenge), [initialChallenge])
   -- reversed [ck + 1, ck + 2, ... cn, 1, ... ck]
-  let challenges = evalState (genChallenges curve vks y msg g h u ss) initialState
+  let challenges = evalState (genChallenges curve pubKeys y msg g h ss) initialState
       revChallenges = reverse challenges
       ck = head challenges
 
+  --------------------- STEP 4 -----------------------
   -- Compute s = u - x * c mod q
-  let s = u - ECDSA.private_d sk * ck `mod` n
+  let s = u - ECDSA.private_d privKey * ck `mod` n
 
   -- [c1, c2, ..., cn]
   let ordChallenges = drop (participants - k) revChallenges <> take (participants - k) revChallenges
@@ -87,6 +89,38 @@ sign curve vks' (vk, sk) msg = do
     g = ECC.ecc_g (ECC.common_curve curve)
     gu u = pointToBS (ECC.pointMul curve u g)
     hu u = pointToBS (ECC.pointMul curve u g)
+
+verify
+  :: ECC.Curve                            -- ^ Curve
+  -> [ECDSA.PublicKey]                    -- ^ List of participants public keys
+  -> (Integer, [Integer], ECC.Point)      -- ^ Signature
+  -> ByteString                           -- ^ Message
+  -> Bool
+verify c pubKeys signature@(c1, ss, y) msg = c1 == c1'
+
+  where
+    gs k s ch =
+      pointToBS $
+        ECC.pointAdd c
+          (ECC.pointMul c s g)
+          (ECC.pointMul c ch (ECDSA.public_q $ pubKeys !! k))
+    hs h k s ch =
+      pointToBS $ ECC.pointAdd c (ECC.pointMul c s h) (ECC.pointMul c ch y)
+    g = ECC.ecc_g (ECC.common_curve c)
+    participants = length pubKeys
+    --------------------- STEP 1 -----------------------
+    -- In ECC, h is a point in the curve. h = g x H_2(L)
+    -- Compute y = h x x_pi
+    h = ECC.pointBaseMul c (hashPubKeys c pubKeys)
+    s = head ss
+    z1 = gs 0 s c1
+    z1' = hs h 0 s c1
+
+    -- initial challenge -> ch_2
+    initialChallenge = genChallenge c pubKeys y msg z1 z1' s
+    initialState = ((1 `mod` participants, s, initialChallenge), [initialChallenge])
+    challenges = evalState (genChallenges c pubKeys y msg g h ss) initialState
+    c1' = head challenges
 
 
 -- | Generate challenge from a given message
@@ -114,21 +148,20 @@ genChallenges
   -> BS.ByteString      -- ^ message msg
   -> ECC.Point          -- ^ generator g
   -> ECC.Point          -- ^ h = H(L)
-  -> Integer            -- ^ u
   -> [Integer]          -- ^ random ss
   -> State ((Int, Integer, Integer), [Integer]) [Integer]
-genChallenges c vks y msg g h u [] = do
+genChallenges c vks y msg g h [] = do
   (_, challenges) <- get
   pure challenges
 
-genChallenges c vks y msg g h u (s:ss) = do
+genChallenges c vks y msg g h (s:ss) = do
   ((prevPos, prevS, prevChallenge), challenges) <- get
   let challenge = nextChallenge prevPos prevS prevChallenge
   modify $ \st ->
       (((prevPos + 1) `mod` length vks, s, challenge)
       , challenge : snd st
       )
-  genChallenges c vks y msg g h u ss
+  genChallenges c vks y msg g h ss
 
   where
     gs prevPos prevS prevChallenge =
