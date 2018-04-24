f382729f82e11a2fd67a8573bbd8e935efba4ca9
Alberto Centelles <centelles.alberto@gmail.com>
2018-04-24 09:59:17 +0100

    Add test to verify signature over SEC curves

--- test/TestLSAG.hs
+++ test/TestLSAG.hs
@@ -6,14 +6,22 @@ import           Test.Tasty
 import           Test.Tasty.HUnit
 import           Test.Tasty.QuickCheck
 import           Crypto.Number.Generate     (generateBetween)
-import qualified Crypto.PubKey.ECC.Prim as ECC
-import qualified Crypto.PubKey.ECC.Types as ECC
+import qualified Crypto.PubKey.ECC.Prim     as ECC
+import qualified Crypto.PubKey.ECC.Types    as ECC
 import qualified Crypto.PubKey.ECC.Generate as ECC
 
 import           LSAG
 
-secp256k1Curve :: ECC.Curve
-secp256k1Curve = ECC.getCurveByName ECC.SEC_p256k1
+newtype Curve = Curve ECC.Curve deriving Show
+
+instance Arbitrary Curve where
+  arbitrary = elements
+    [ Curve $ ECC.getCurveByName ECC.SEC_p192r1
+    , Curve $ ECC.getCurveByName ECC.SEC_p256k1
+    , Curve $ ECC.getCurveByName ECC.SEC_p256r1
+    , Curve $ ECC.getCurveByName ECC.SEC_p384r1
+    ]
+
 
 -- | Insert element at specified position
 insert :: Int -> a -> [a] -> [a]
@@ -21,17 +29,17 @@ insert k e l = take k l <> [e] <> drop k l
 
 testLSAG :: TestTree
 testLSAG = testGroup "LSAG Signature"
-  [ localOption (QuickCheckTests 10) $ testProperty
-      "Verify signature on curve SECp256k1"
-      (forAll (choose (3, 50)) (testSignature secp256k1Curve))
+  [ localOption (QuickCheckTests 20) $ testProperty
+      "Verify signature on SEC curves"
+      (forAll (choose (3, 50)) testSignature)
   ]
 
 testSignature
-  :: ECC.Curve
-  -> Int
+  :: Int
+  -> Curve
   -> [Char]
   -> Property
-testSignature curve nParticipants msg = monadicIO $ do
+testSignature nParticipants (Curve curve) msg = monadicIO $ do
   -- Gen public and private keys
   (pubKey, privKey) <- liftIO $ ECC.generate curve
   -- Random foreign participants
