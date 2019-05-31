29dfcdd592afcb571f501c71d1674d5689898912
Alberto Centelles <centelles.alberto@gmail.com>
2019-05-31 12:27:13 +0100

    Hide discrete logarithm k such that H = kG

--- src/LSAG.hs
+++ src/LSAG.hs
@@ -29,6 +29,7 @@ import qualified Data.ByteString              as BS
 import           Data.Monoid
 import           Data.List
 import           Protolude                    hiding (hash, head)
+import Math.NumberTheory.Moduli.Sqrt (sqrtModP)
 
 -- | Generates a ring signature for a message given a specific set of
 -- public keys and a signing key belonging to one of the public keys
@@ -82,8 +83,8 @@ sign pubKeys (pubKey, privKey) msg =
 
   where
     curve = ECDSA.public_curve pubKey
-    -- h = [Hash(L)] * g
-    h = ECC.pointBaseMul curve (hashPubKeys curve pubKeys)
+    -- h = [Hash(L)]
+    h = generateH g curve (show $ hashPubKeys curve pubKeys)
     -- y = [x] * h
     y = ECC.pointMul curve (ECDSA.private_d privKey) h
     n = ECC.ecc_n (ECC.common_curve curve)
@@ -116,8 +117,10 @@ verify pubKeys (ch0, [s], y) msg = panic "Invalid input"
 verify pubKeys (ch0, s0:s1:s2ToEnd, y) msg = ch0 == ch0'
   where
     curve0 = ECDSA.public_curve $ head pubKeys
-    -- h = [H(L)] * g
-    h = ECC.pointBaseMul curve0 (hashPubKeys curve0 pubKeys)
+    -- h = [H(L)]
+    h = generateH g curve0 (show $ hashPubKeys curve0 pubKeys)
+
+    -- h = ECC.pointBaseMul curve0 (hashPubKeys curve0 pubKeys)
     y0 = ECDSA.public_q $ head pubKeys
     -- z0' = [s0] * g + [ch0] * y0
     z0' = ECC.pointAdd curve0
@@ -157,7 +160,7 @@ genChallenges pubKeys y msg ss = do
       genChallenges pubKeys y msg ss
   where
     g curve = ECC.ecc_g (ECC.common_curve curve)
-    h curve = ECC.pointBaseMul curve (hashPubKeys curve pubKeys)
+    h curve = generateH (g curve) curve (show $ hashPubKeys curve pubKeys)
     gs curve prevK prevS prevCh =
       ECC.pointAdd curve
         (ECC.pointMul curve prevS (g curve))
@@ -203,6 +206,25 @@ pointToBS (ECC.Point x y) = show x <> show y
 -- | Convert list of public keys to bytestring
 pubKeysToBS :: [ECDSA.PublicKey] -> BS.ByteString
 pubKeysToBS = foldMap (pointToBS . ECDSA.public_q)
+
+
+-- | Iterative algorithm to generate H.
+-- The important to hide its discrete logarithm "k" such that H = kG
+generateH :: ECC.Point -> ECC.Curve -> [Char] -> ECC.Point
+generateH g curve extra =
+  case yM of
+    Nothing -> generateH g curve (toS $ '1':extra)
+    Just y -> if ECC.isPointValid curve (ECC.Point x y)
+      then ECC.Point x y
+      else generateH g curve (toS $ '1':extra)
+  where
+    x = oracle curve (pointToBS g <> toS extra) `mod` p
+    yM = sqrtModP (x ^ 3 + 7) p
+    p = ECC.ecc_p cp
+      where
+        cp = case curve of
+               ECC.CurveFP c -> c
+               ECC.CurveF2m _ -> panic "Not a FP curve"
 
 -- | Hash list of public keys
 hashPubKeys :: ECC.Curve -> [ECDSA.PublicKey] -> Integer
